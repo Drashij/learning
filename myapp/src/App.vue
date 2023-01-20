358e759b6bb261dfe1800ace459127425e8e273f
Drashi Jain <123073865+Drashij@users.noreply.github.com>
2023-01-20 11:34:21 +0530

    Add files via upload

---
+++ myapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Ninjas from "./components/Ninja.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
+  },
+  data() {
+    return {
+      //title: "vue Wizards"
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
---
+++ myapp/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: "Copyright @ 2023"
+    };
+  }
+};
+</script>
+<style scoped>
+footer {
+  background: #222;
+  padding: 6px;
+  margin-top: 180px;
+}
+p {
+  color: lightgreen;
+  text-align: center;
+}
+</style>
